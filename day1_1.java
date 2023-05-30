ff86d434560eb71dcaee7c3f87447daa5031434f
aaditya shukla <131380763+Aaditya14041998@users.noreply.github.com>
2023-05-30 08:26:16 +0530

    Add files via upload

---
+++ day1_1.java
@@ -0,0 +1,14 @@
+
+public class day1_1 {
+
+	public static void main(String[] args) {
+		int s = 0;
+		for (int i = 0; i < args.length; i++) {
+			s += Integer.parseInt(args[i]);
+		}
+
+		
+		System.out.println("Addition:"+s);
+	}
+
+}
